401d11b9e4bf2db42a56a2c850049f57df9bc37e
NemoBJJ <engnfaraujo@gmail.com>
2026-07-29 17:16:40 -0300

    Migrando Estoque para Contabo

--- src/api.js
+++ src/api.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
-// Forçando novo build do FrontEnd - 11/07/2026
-console.log('🚀 FrontEnd Estoque - Conectando ao BackEnd Render');
+// FrontEnd Estoque - Contabo VPS
+console.log('🚀 FrontEnd Estoque - Conectando ao BackEnd Contabo');
 
-const isDevelopment = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+const isDevelopment =
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '127.0.0.1';
 
 const BASE_URL = isDevelopment
   ? 'http://localhost:8083/api'
-  : 'https://estoque-api-rfhv.onrender.com/api';
+  : 'https://estoque-container.neemindev.com/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
